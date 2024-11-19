39488b9efa32010854e731c37e6cedc85587508e
Jared Long <jlong4223@gmail.com>
2024-11-19 12:10:54 -0600

    adds semis for prettier

--- src/getRawCollectionData.ts
+++ src/getRawCollectionData.ts
@@ -1,18 +1,26 @@
-import { Paginate, Documents, Collection, Lambda, Get, Var, Map } from 'faunadb'
-import { faunaClient } from './setFaunaSecret'
-import { RawDocumentRefs } from './types/types'
+import {
+	Paginate,
+	Documents,
+	Collection,
+	Lambda,
+	Get,
+	Var,
+	Map
+} from 'faunadb';
+import { faunaClient } from './setFaunaSecret';
+import { RawDocumentRefs } from './types/types';
 
 export const getRawCollectionDocData = async (
 	collection: string,
 	size?: number
 ): Promise<RawDocumentRefs> => {
-	const defaultSize = 1000
-	const querySize = (size ||= defaultSize)
+	const defaultSize = 1000;
+	const querySize = (size ||= defaultSize);
 
 	return await faunaClient.query(
 		Map(
 			Paginate(Documents(Collection(collection)), { size: querySize }),
 			Lambda('ref', Get(Var('ref')))
 		)
-	)
-}
+	);
+};
