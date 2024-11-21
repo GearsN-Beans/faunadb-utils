c590f429d7187a23c96c986d9615e7c07062c8d3
Jared Long <jlong4223@gmail.com>
2024-11-20 21:25:04 -0600

    feat: adds env & tests

---
+++ src/__tests__/faunav10.test.ts
@@ -0,0 +1,44 @@
+import { describe, test, expect, beforeEach } from 'vitest';
+import { faunaClientV10, setFaunaSecretV10 } from '../v10/setFaunaSecret';
+import { getRawCollectionData } from '../v10/getRawCollectionData';
+import { Client } from 'fauna';
+
+const faunaSecret = import.meta.env.VITE_V10_FAUNA_SECRET;
+
+describe('faunaClientV10', () => {
+	test('faunaClientV10 before set', () => {
+		expect(faunaClientV10).toBe(undefined);
+	});
+
+	test('faunaClientV10 after set', () => {
+		setFaunaSecretV10(faunaSecret);
+		expect(faunaClientV10).not.toBe(undefined);
+		expect(faunaClientV10).toBeInstanceOf(Client);
+	});
+});
+
+describe('all functions tests', () => {
+	beforeEach(() => {
+		setFaunaSecretV10(faunaSecret);
+	});
+
+	describe('getRawCollectionDocData', () => {
+		test('should return an array of data', async () => {
+			const productData = await getRawCollectionData('Product');
+
+			expect(productData.data.data.length).toBeGreaterThan(0);
+		});
+
+		test('should return a correct length with the size param passed in', async () => {
+			const { data: productData } = await getRawCollectionData('Product', 1);
+
+			expect(productData.data).toHaveLength(1);
+		});
+
+		test('return an empty array with an empty collection', async () => {
+			const emptyCollectionData = await getRawCollectionData('EmptyCollection');
+
+			expect(emptyCollectionData.data.data).toHaveLength(0);
+		});
+	});
+});
--- vite-env.d.ts
+++ vite-env.d.ts
@@ -1,8 +1,9 @@
 interface ImportMetaEnv {
-	VITE_FAUNA_SECRET: string
+	VITE_FAUNA_SECRET: string;
+	VITE_V10_FAUNA_SECRET: string;
 	// Add other environment variables here...
 }
 
 interface ImportMeta {
-	env: ImportMetaEnv
+	env: ImportMetaEnv;
 }
